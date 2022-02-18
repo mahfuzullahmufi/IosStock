4f7d2631392c4d0483caa04ace0bb22ee1ec62b7
mahfuzullahmufi <mahfuzullah15-11006@diu.edu.bd>
2022-02-18 21:37:22 +0600

    first commit

---
+++ IosStockWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using IosStockWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IosStockWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
